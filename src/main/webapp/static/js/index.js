4d528b14f0e9dd3616d30fe4523920ba687943eb
zxd <501801307@qq.com>
2017-09-15 10:04:52 +0800

    前端页面构建

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 	// 让用户头像呈现为正方形。。。
     $('#user_picture').css('height',$('#user_picture').css('width'));
     $('#user_picture img').css('width',$('#user_picture').css('width'));
-
+    to_page(1);
 })
 
 
-// 保存message信息的全局变量
-var messages;
-
+// 把时间戳变为普通日期
+function getLocalTime(nS) {     
+   return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');     
+} 
 
 
 // 根据传入的页码号，更新message这个全局变量
@@ -20,18 +21,26 @@ function to_page(argument) {
 		data: {'pn': argument},
 		success:function(result){
 
-			messages=result;
+			var vm_messages = new Vue({
+
+			el: "#app-messages",
+			data:{
+				'vue_msg':result.extend.pageinfo.list,
+			}
+
+			})
 		}
 	});	
+}
+
 
 
 // 用vue渲染列表，并绑定数据
-var vm_messages = new Vue({
+// var vm_messages = new Vue({
 
-	el: "app-messages",
-	data:{
+// 	el: "#app-messages",
+// 	data:{
+// 		'vue_msg':messages.extend.pageinfo.list
+// 	}
 
-		vue_msg = messages;
-	}
-
-})
+// })
